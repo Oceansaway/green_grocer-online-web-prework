f03861094cc559a5307fbab5f9456c8603833c31
Stephanie Purple <spurple133@gmail.com>
2019-04-15 00:40:37 +0000

    Automatically backed up by Learn

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -3,8 +3,8 @@ def consolidate_cart(cart:[])
   cart.each do |item|
     item.each do |name, attribute|
       if con_hash.has_key?(name)
-        con_hash[name][:count] += 1        
-      else 
+        con_hash[name][:count] += 1
+      else
         con_hash = con_hash.merge({name => attribute.merge({count: 1})})
       end
     end
